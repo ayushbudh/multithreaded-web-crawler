ce01a00a9eb9987a63386a87e216296151b3bbcc
valentinaaranggo <valea28@gmail.com>
2023-11-22 12:50:30 -0500

    0.2.0
    Created mutithreaded webcrawler that remembers the last URLs and is able
    to resume, and is able to create appropriate number of threads. All of
    these requirements were tested using unit tests in the BotTest.java file
    overall the Bot class is the implementation of the webcrawler  and
    contains the logic for crawling web pages while Main class file is
    designed to test the webcrawler. comments are provided for further
    explanation of the methods used.

---
+++ src/main/BotTest.java
@@ -0,0 +1,40 @@
+package src.main;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+class BotTest {
+    @Test
+    void testCrawlerWithResume() {
+        // Create an instance of Bot
+        Bot bot = new Bot("https://dictionary.com", 2, 3);
+        bot.startBot();
+        // Create a new instance of Bot for resuming
+        Bot resumedBot = new Bot("https://dictionary.com", 2, 3);
+        // Start the resumed bot, which should resume from the previous state
+        resumedBot.startBot();
+        // Check if the two instances have visited the same number of links
+        assertEquals(bot.getVisitedLinks().size(), resumedBot.getVisitedLinks().size());
+    }
+
+    @Test
+    void testAppropriateNumberOfThreads() {
+        // Create an instance of Bots
+        Bot bot = new Bot("https://dictionary.com", 2, 3);
+        bot.startBot();
+        // Check if the number of threads used by the bot matches the specified number
+        assertEquals(3, bot.getNumThreads());
+    }
+
+    @Test
+    void testWebCrawler() {
+        // Create an instance of Bot
+        Bot bot = new Bot("https://dictionary.com", 2, 3);
+        bot.startBot();
+
+        // Check if the visited links set is not empty
+        assertFalse(bot.getVisitedLinks().isEmpty());
+    }
+}
