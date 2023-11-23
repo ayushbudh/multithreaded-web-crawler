d994ad40e1dcef112f42ab1835d94b18176fd0d3
ayushbudh <ayushbudhwani41@gmail.com>
2023-11-23 02:04:40 -0500

    refactor code, remove test, and add analytics_report

--- webCrawler/src/main/Main.java
+++ webCrawler/src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
+// Main class is designed to test the webcrawler
 public class Main {
-	// Main class is designed to test the webcrawler
-  public static void main( String[] args) {
-	  int maxDepth=2;
-	  int numThreads=3;
-	
-	   Bot bot= new Bot("https://dictionary.com",maxDepth, numThreads);
-	   
-	   bot.startBot();
-}
+	public static void main(String[] args) {
+		int maxDepth = 2;
+		int numThreads = 2;
+		String URLS[] = { "https://dictionary.com" };
+
+		SequentialWebCrawler sequentialWebCrawler = new SequentialWebCrawler(URLS, maxDepth);
+		sequentialWebCrawler.startCrawler();
+
+		MultithreadedWebCrawler multithreadedWebCrawler = new MultithreadedWebCrawler(URLS, maxDepth, numThreads);
+		multithreadedWebCrawler.startCrawler();
+
+	}
 }
